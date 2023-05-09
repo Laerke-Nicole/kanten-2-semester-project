f93695caac4c4d7278f63028078b0659245aa41f
Lærke Nielsen <laenie01@easv365.dk>
2023-05-09 20:34:54 +0200

    Deleting uunnecessary pages that vue created in setup + adding database

--- kanten-web/src/firebase/index.js
+++ kanten-web/src/firebase/index.js
@@ -3,6 +3,9 @@ import { initializeApp } from 'firebase/app';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+// Cloud firestone
+import { getFirestore } from 'firebase/firestore';
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: 'AIzaSyAoKtUqjD90pJeNV27dMc_yTE9jCtkOe5U',
@@ -15,3 +18,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export {
+  db
+}
